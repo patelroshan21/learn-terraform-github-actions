bf4452d97ec8b3369c0f1ec9b2bc4f1c03196912
Roshan Patel <patelroshan21@yahoo.com>
2020-12-07 13:29:50 -0800

    initial commit for github actions

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "mavenlink"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "ec2-dev"
     }
   }
 }
